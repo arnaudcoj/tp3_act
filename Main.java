00bb63b14a0cf3daaa972f11a7d7947ec8f6e03c
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2015-10-14 09:42:33 +0200

    doing dynamic but initConfigurationTable > segfault

--- Main.java
+++ Main.java
@@ -7,34 +7,46 @@ import java.util.List;
  *
  */
 
-
 public class Main {
+
+    public static int[][][][] configurations = null;
+
     public static void main(String[] args) {
 	/*List<Integer> list = new ArrayList<Integer>();
-	list.add(5);
-	System.out.println(valPreviousPos(list));
-	list.add(3);
-	System.out.println(valPreviousPos(list));
-	list.add(0);
-	System.out.println(valPreviousPos(list));
-	list.add(-4);
-	System.out.println(valPreviousPos(list));
-	list.add(-1);
-	System.out.println(valPreviousPos(list));*/
+	  list.add(5);
+	  System.out.println(valPreviousPos(list));
+	  list.add(3);
+	  System.out.println(valPreviousPos(list));
+	  list.add(0);
+	  System.out.println(valPreviousPos(list));
+	  list.add(-4);
+	  System.out.println(valPreviousPos(list));
+	  list.add(-1);
+	  System.out.println(valPreviousPos(list));*/
 
 	
-	System.out.println(winRate(1,1,1,1));
-	System.out.println(winRate(2,1,2,1));
-	System.out.println(winRate(1,2,1,2));
-	System.out.println(winRate(2,2,2,2));
-	System.out.println(winRate(3,2,3,2));
-	System.out.println(winRate(3,1,3,1));
-	System.out.println(winRate(3,2,3,2));
+	initConfigurationTable(1,1,1,1);
+	initConfigurationTable(2,1,2,1);
+	initConfigurationTable(1,2,1,2);
+	initConfigurationTable(2,2,2,2);
+	initConfigurationTable(3,2,3,2);
+	initConfigurationTable(3,1,3,1);
+	initConfigurationTable(3,2,3,2);
+
+
+	/*
+	  System.out.println(winRate(1,1,1,1));
+	  System.out.println(winRate(2,1,2,1));
+	  System.out.println(winRate(1,2,1,2));
+	  System.out.println(winRate(2,2,2,2));
+	  System.out.println(winRate(3,2,3,2));
+	  System.out.println(winRate(3,1,3,1));
+	  System.out.println(winRate(3,2,3,2));*/
     }
 
     
     
-    static int valPreviousPos(List<Integer> list) {
+    static public int valPreviousPos(List<Integer> list) {
 	//maybe do a try catch to check if the list is empty or not?
 	int val = list.get(0);
 	for (int i=1; i<list.size(); i++) {
@@ -72,7 +84,10 @@ public class Main {
 	}
     }
 
-    static int winRate(int width, int height, int widthSkull, int heightSkull) {
+    public static int winRate(int width, int height, int widthSkull, int heightSkull) {
+	if(configurations == null || configurations.length < width || configurations[0].length < height || configurations[0][0].length < widthSkull || configurations[0][0][0].length < heightSkull)
+	    initConfigurationTable(width, height, widthSkull, heightSkull);
+	
 	List<Integer> chocolateBar = new ArrayList<Integer>(0);
 	//if juste one square
 	if ( width == 1 && height == 1) {
@@ -80,22 +95,46 @@ public class Main {
 	}
 
 	//else calculate all the different possibilities of cutting
-	//cut by the left
-	for (int k=1;k<widthSkull;k++) {
-	    chocolateBar.add(winRate(width-k, height, widthSkull-k, heightSkull));
+
+	if(configurations[width-1][height-1][widthSkull][heightSkull] < 0) {
+	    //cut by the left
+	    for (int k=1;k<widthSkull;k++) {
+		chocolateBar.add(winRate(width-k, height, widthSkull-k, heightSkull));
+	    }
+	    //cut by the up
+	    for (int f=1;f<heightSkull;f++) {
+		chocolateBar.add(winRate(width, height-f, widthSkull, heightSkull-f));
+	    }
+	    //cut by the right
+	    for (int q=1;q<width-widthSkull-1;q++) {
+		chocolateBar.add(winRate(width-q, height, widthSkull, heightSkull));
+	    }
+	    //cut by the down
+	    for (int u=1;u<height-heightSkull-1;u++) {
+		chocolateBar.add(winRate(width, height-u, widthSkull, heightSkull));
+	    }
+
+	    configurations[width-1][height-1][widthSkull][heightSkull] = valPreviousPos(chocolateBar);
 	}
-	//cut by the up
-	for (int f=1;f<heightSkull;f++) {
-	    chocolateBar.add(winRate(width, height-f, widthSkull, heightSkull-f));
-	}
-	//cut by the right
-	for (int q=1;q<width-widthSkull-1;q++) {
-	    chocolateBar.add(winRate(width-q, height, widthSkull, heightSkull));
-	}
-	//cut by the down
-	for (int u=1;u<height-heightSkull-1;u++) {
-	    chocolateBar.add(winRate(width, height-u, widthSkull, heightSkull));
-	}
-	return valPreviousPos(chocolateBar);
+	
+	return configurations[width-1][height-1][widthSkull][heightSkull];
     }
+
+    public static void initConfigurationTable(int width, int height, int widthSkull, int heightSkull) {
+	configurations = new int[width][height][widthSkull][heightSkull];
+	for(int i = 0; i < width; i++) {
+	    //	    configurations[i] = new int[][][][](height);
+	    for(int j = 0; j < height; j++) {
+		//configurations[i][j] = new int[][][][](widthSkull);
+		for(int k = 0; k < widthSkull; k++) {
+		    //  configurations[i][j][k] = new int[][][][](heightSkull);
+		    for(int l = 0; l < widthSkull; l++) {
+			configurations[i][j][k][l] = -1;
+		    }		    
+		}
+	    }
+	}
+	
+    }
+    
 }
